de438d26d5c28eaf76046846a9bf2928e027da59
TheMegax <mathias.tello12@gmail.com>
2023-07-27 14:26:48 -0400

    Part 1

--- src/app/material-component/mis-datos/mis-datos.component.ts
+++ src/app/material-component/mis-datos/mis-datos.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { DemoMaterialModule } from 'src/app/demo-material-module';
+import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+
+interface Gender {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-mis-datos',
   standalone: true,
-  imports: [DemoMaterialModule],
+  imports: [DemoMaterialModule, NgForOf, FormsModule],
   templateUrl: './mis-datos.component.html',
   styleUrls: ['./mis-datos.component.scss']
 })
+
 export class MisDatosComponent {
-  panelOpenState = false;
-  step = 0;
+  step: number = 0;
+  disabled = false;
 
   setStep(index: number) {
     this.step = index;
@@ -24,4 +32,10 @@ export class MisDatosComponent {
   prevStep() {
     this.step--;
   }
+
+  genders: Gender[] = [
+    {value: 'MASCULINO', viewValue: 'Masculino'},
+    {value: 'FEMENINO', viewValue: 'Femenino'},
+    {value: 'TRANSGENERO', viewValue: 'Otro'},
+  ];
 }
