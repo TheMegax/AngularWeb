b467a140ba494266ad7865e5a71fabf893653506
Rodrigo Chang <rodrigo.chang@gmail.com>
2023-07-28 16:34:17 -0400

    API OK on Mis Datos

--- src/app/material-component/mis-datos/mis-datos.component.ts
+++ src/app/material-component/mis-datos/mis-datos.component.ts
@@ -3,6 +3,10 @@ import { DemoMaterialModule } from 'src/app/demo-material-module';
 import {NgForOf, NgIf} from "@angular/common";
 import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {FhirApiService} from "../../service/fhir-api.service";
+import {AuthService} from "../../service/auth.service";
+import {environment} from "../../../environments/environment";
+import axios from 'axios';
 
 
 interface InternalString {
@@ -19,80 +23,30 @@ interface InternalString {
 })
 
 export class MisDatosComponent {
-  constructor(public snackBar: MatSnackBar) {}
+  data: any;
 
-  step: number = 1;
-  disabled = false;
+  constructor(private fhirApiService: FhirApiService, private authService: AuthService) {}
 
-  setStep(index: number) {
-    this.step = index;
+  ngOnInit() {
+    this.getTokenAndGetData();
   }
 
-  nextStep() {
-    this.step++;
+  private getTokenAndGetData() {
+    this.authService.getToken().subscribe((response: any) => {
+      const token = response.access_token;
+      console.log(token);
+      environment.apikey = token;
+      environment.oauthauth = token;
+
+      // Call getDataByRun here to ensure it's executed after getting the token
+      this.getDataByRun();
+    });
   }
 
-  prevStep() {
-    this.step--;
-  }
-
-  genders: InternalString[] = [
-    {value: 'MASCULINO', viewValue: 'Masculino'},
-    {value: 'FEMENINO', viewValue: 'Femenino'},
-    {value: 'TRANSGENERO', viewValue: 'Otro'},
-  ];
-
-  vias: InternalString[] = [
-    {value: 'CALLE', viewValue: 'Calle'},
-    {value: 'AVENIDA', viewValue: 'Avenida'},
-  ];
-
-  regiones: InternalString[] = [
-    {value: 'RM', viewValue: 'Región Metropolitana de Santiago'},
-    {value: 'AP', viewValue: 'Región de Arica y Parinacota'},
-    {value: 'TA', viewValue: 'Región de Tarapacá'},
-    {value: 'AN', viewValue: 'Región de Antofagasta'},
-    {value: 'AT', viewValue: 'Región de Atacama'},
-    {value: 'CO', viewValue: 'Región de Coquimbo'},
-    {value: 'VA', viewValue: 'Región de Valparaíso'},
-    {value: 'LI', viewValue: 'Región del Libertador General Bernardo O\'Higgins'},
-    {value: 'ML', viewValue: 'Región de Maule'},
-    {value: 'NB', viewValue: 'Región de Ñuble'},
-    {value: 'BI', viewValue: 'Región de Biobío'},
-    {value: 'AR', viewValue: 'Región de La Araucanía'},
-    {value: 'LR', viewValue: 'Región de Los Ríos'},
-    {value: 'LL', viewValue: 'Región de Los Lagos'},
-    {value: 'AI', viewValue: 'Región de Aysén del General Carlos Ibáñez del Campo'},
-    {value: 'MA', viewValue: 'Región de Magallanes y de la Antártica Chilena'},
-  ];
-
-  req_nombres = new FormControl({value: '', disabled: true}, [Validators.required])
-  req_apellido1 = new FormControl({value: '', disabled: true}, [Validators.required])
-  req_apellido2 = new FormControl({value: '', disabled: true}, [Validators.required])
-  req_edad = new FormControl({value: '', disabled: true}, [Validators.required])
-  req_rut = new FormControl({value: '', disabled: true}, [Validators.required])
-  req_sexo = new FormControl({value: '', disabled: true}, [Validators.required])
-  req_nombre_social = new FormControl({value: '', disabled: true})
-  req_telefono = new FormControl('', [Validators.required])
-  req_via = new FormControl('', [Validators.required])
-  req_via_nombre = new FormControl('', [Validators.required])
-  req_via_numero = new FormControl('', [Validators.required])
-  req_region = new FormControl('', [Validators.required])
-  req_ciudad = new FormControl('', [Validators.required])
-  req_comuna = new FormControl('', [Validators.required])
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'Debes entrar un valor';
-    }
-
-    return this.email.hasError('email') ? 'No un email válido' : '';
-  }
-  openSnackBar(message: string, action: string) {
-    this.snackBar.open(message, action, {
-      duration: 2000,
+  getDataByRun() {
+    this.fhirApiService.getPatientByRUN('666666666-14').then((res) => {
+      console.log(res);
+      this.data = res.data;
     });
   }
 }
